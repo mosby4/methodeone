93d08eb00f08b1d41f5d1d7e097ed97650d81cb5
rufus hamby <rufus.hamby@gmail.com>
2022-07-06 15:15:19 -0500

    Completed exercises

--- methodeone/Program.cs
+++ methodeone/Program.cs
@@ -4,8 +4,22 @@ namespace MethodeOne
 {
     class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
         static void Main(String[] args)
         {
+
+            var amountOfcars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+
             //----------------exercise 1
             // Name: Michael
             // Favorite Color: Blue
@@ -31,6 +45,10 @@ namespace MethodeOne
             Console.WriteLine($"Favorite color {favColor}");
             Console.WriteLine($"Favorite animal {animal}");
             Console.WriteLine($"Favorite band {band}");
+
+            //-----------------Exercise 2---------------------------------//
+
+
 
             
 
